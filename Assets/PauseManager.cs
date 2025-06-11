6a4c1530467e90c6109b26eb51dfec21d89f5349
Siskaaaaaa <siskaikaa09@gmail.com>
2025-06-11 08:50:38 +0700

    pause

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool isPaused;
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
