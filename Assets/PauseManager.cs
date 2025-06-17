8b710f1cb8052e773784c8763ab012ea980e93bf
Siskaaaaaa <siskaikaa09@gmail.com>
2025-06-18 06:55:49 +0700

    benarkan pause

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
     public bool isPaused;
+    public GameObject pauseMenu; // Assign panel PauseMenu dari Canvas
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
     public void TogglePause()
     {
@@ -11,10 +19,31 @@ public class PauseManager : MonoBehaviour
         if (isPaused)
         {
             Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
         }
         else
         {
             Time.timeScale = 1f;
+            pauseMenu.SetActive(false);
         }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void GoToHome()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); // ganti sesuai nama scene
+    }
+
+    public void GoToLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level2"); // ganti sesuai nama scene
     }
 }
